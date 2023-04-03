6d9501494baf47f80ae4350b33e169d1c8376c06
nitzan <nitzantomer1998@gmail.com>
2023-04-03 15:19:21 +0300

    Fixing errors

--- src/useFetch.jsx
+++ src/useFetch.jsx
@@ -1,33 +1,33 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 export const useFetch = (url) => {
-    const [data, setData] = useState(null)
-    const [isPending, setIsPending] = useState(true)
-    const [error, setError] = useState(null)
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
-        const abortCont = AbortController
+        const abortCont = AbortController;
         fetch(url, { signal: abortCont.signal })
             .then(res => {
                 if (!res.ok) {
-                    throw Error("could not fetch the data for that resource")
+                    throw Error("could not fetch the data for that resource");
                 }
                 return res.json();
 
             })
             .then((data) => {
-                setData(data)
-                setIsPending(false)
-                setError(null)
+                setData(data);
+                setIsPending(false);
+                setError(null);
             })
             .catch((err) => {
                 if (!err.name === 'Aborterror') {
-                    setIsPending(false)
-                    setError(err.message)
+                    setIsPending(false);
+                    setError(err.message);
                 }
-            })
+            });
         return () => abortCont.abort();
-    }, [url])
+    }, [url]);
 
-    return { data, isPending, error }
-}
+    return { data, isPending, error };
+};
