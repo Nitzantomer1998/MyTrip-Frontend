fd9c78789ba67e6d85cc5d0da2427252586014a0
nitzan <nitzantomer1998@gmail.com>
2023-04-04 08:33:56 +0300

    Installing mocha and cahi

---
+++ testss/Array.spec.js
@@ -0,0 +1,12 @@
+import { expect } from "chai";
+
+import { expect } from "chai";
+
+describe("Arrays", () => {
+  describe("sort", () => {
+    it("Sort not sorted names array", () => {
+      var names = ["Dani", "Moshe", "Adam"];
+      expect(names.sort()).to.be.eql(["Adam", "Dani", "Moshe"]);
+    });
+  });
+});
