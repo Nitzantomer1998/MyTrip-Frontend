ad1a74d28f08be26e39e7fc3641d637d44b93464
nitzan <nitzantomer1998@gmail.com>
2023-04-04 08:09:57 +0300

    Starting fresh

--- src/pages/AfterLogin/AfterLogin.jsx
+++
@@ -1,7 +0,0 @@
-export const AfterLogin = () => {
-    return (
-        <div>
-            <h2>You have logged in successfuly</h2>
-        </div>
-    );
-};
--- src/pages/Home/Home.jsx
+++
@@ -1,7 +0,0 @@
-export const Home = () => {
-    return (
-        <div className="home">
-            <h2>Home</h2>
-        </div>
-    );
-};
