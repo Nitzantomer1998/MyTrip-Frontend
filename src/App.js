0e0d50ce219aeca98f1170e87d96764b3b679b64
Nitzan <nitzantomer1998@gmail.com>
2023-04-11 17:27:27 +0300

    Adjusting postfix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import React, { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar.jsx';
+import Navbar from './components/Navbar/Navbar.js';
 
-const Home = lazy(() => import('./pages/Home/Home.jsx'));
-const Login = lazy(() => import('./pages/Login/Login.jsx'));
-const Register = lazy(() => import('./pages/Register/Register.jsx'));
-const AfterLogin = lazy(() => import('./pages/AfterLogin/AfterLogin.jsx'));
+const Home = lazy(() => import('./pages/Home/Home.js'));
+const Login = lazy(() => import('./pages/Login/Login.js'));
+const Register = lazy(() => import('./pages/Register/Register.js'));
+const AfterLogin = lazy(() => import('./pages/AfterLogin/AfterLogin.js'));
 const AfterRegister = lazy(() =>
-  import('./pages/AfterRegister/AfterRegister.jsx')
+  import('./pages/AfterRegister/AfterRegister.js')
 );
 
 function App() {
--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,7 +1,7 @@
 import './Register.css';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { validateLoginForm } from '../../validation/userValidation.jsx';
+import { validateLoginForm } from '../../validation/userValidation.js';
 
 function Register() {
   const navigate = useNavigate();
