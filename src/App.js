ec9bee9d7883d372dda2ed9728927f40a2797bd7
nitzan <nitzantomer1998@gmail.com>
2023-04-21 11:02:32 +0300

    Updating new routes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import './App.css';
-import React, { lazy, Suspense } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar.js';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-const Home = lazy(() => import('./pages/Home/Home.js'));
-const Login = lazy(() => import('./pages/Login/Login.js'));
-const Register = lazy(() => import('./pages/Register/Register.js'));
-const AfterLogin = lazy(() => import('./pages/AfterLogin/AfterLogin.js'));
-const AfterRegister = lazy(() =>
-  import('./pages/AfterRegister/AfterRegister.js')
-);
+import SignIn from './pages/SignIn/SignIn.js';
+import SignUp from './pages/SignUp/SignUp.js';
+import AfterSignIn from './pages/AfterSignIn/AfterSignIn.js';
+import AfterSignUp from './pages/AfterSignUp/AfterSignUp.js';
 
 function App() {
   return (
     <>
-      <Navbar />
-
       <div className='App'>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/afterlogin' element={<AfterLogin />} />
-            <Route path='/afterregister' element={<AfterRegister />} />
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route path='/' element={<SignUp />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/after-sign-ip' element={<AfterSignIn />} />
+          <Route path='/after-sign-up' element={<AfterSignUp />} />
+        </Routes>
       </div>
     </>
   );
