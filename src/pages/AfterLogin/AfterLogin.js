919a3bbe2bb791a313a18b3fbe33be575218e768
nitzan <nitzantomer1998@gmail.com>
2023-04-21 11:03:09 +0300

    Deleting undeeded files

--- src/pages/AfterLogin/AfterLogin.js
+++
@@ -1,9 +0,0 @@
-function AfterLogin() {
-  return (
-    <div>
-      <h2>You have successfully logged in</h2>
-    </div>
-  );
-}
-
-export default AfterLogin;
--- src/pages/Home/Home.js
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div className='home'>
-      <h2>Home</h2>
-    </div>
-  );
-}
-
-export default Home;
