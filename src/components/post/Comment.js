312a80f8d861272eb9c927b764a92292824062b4
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-05-16 08:51:39 +0300

    Update Comment.js

--- src/components/post/Comment.js
+++ src/components/post/Comment.js
@@ -1,21 +1,18 @@
-import Moment from "react-moment";
+import Moment from 'react-moment';
 
 export default function Comment({ comment }) {
   return (
-    <div className="comment">
-      <img src={comment.commentBy.picture} alt="" className="comment_img" />
-      <div className="comment_col">
-        <div className="comment_wrap">
-          <div className="comment_name">
-            {comment?.commentBy?.username}
-          </div>
-          <div className="comment_text">{comment.comment}</div>
+    <div className='comment'>
+      <img src={comment.commentBy.picture} alt='' className='comment_img' />
+      <div className='comment_col'>
+        <div className='comment_wrap'>
+          <div className='comment_name'>{comment?.commentBy?.username}</div>
+          <div className='comment_text'>{comment.comment}</div>
         </div>
         {comment.image && (
-          <img src={comment.image} alt="" className="comment_image" />
+          <img src={comment.image} alt='' className='comment_image' />
         )}
-        <div className="comment_actions">
-          
+        <div className='comment_actions'>
           <span>
             <Moment fromNow interval={30}>
               {comment.commentAt}
