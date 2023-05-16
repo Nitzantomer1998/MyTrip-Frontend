f42dd820da8d4273c64c636b477297d3dc2f8e51
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-05-16 08:51:13 +0300

    Update MenuItem.js

--- src/components/post/MenuItem.js
+++ src/components/post/MenuItem.js
@@ -1,10 +1,10 @@
 export default function MenuItem({ icon, title, subtitle, img }) {
   return (
-    <li className="hover1">
-      {img ? <img src={img} alt="" /> : <i className={icon}></i>}
-      <div className="post_menu_text">
+    <li className='hover1'>
+      {img ? <img src={img} alt='' /> : <i className={icon}></i>}
+      <div className='post_menu_text'>
         <span>{title}</span>
-        {subtitle && <span className="menu_post_col">{subtitle}</span>}
+        {subtitle && <span className='menu_post_col'>{subtitle}</span>}
       </div>
     </li>
   );
