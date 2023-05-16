fd3441552bd07685daca06476ce0b7341d3f3d9f
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-05-16 09:56:46 +0300

    Adding data folder

---
+++ src/data/home.js
@@ -0,0 +1,25 @@
+export const left = [
+  {
+    text: 'Home',
+    img: 'friends',
+    path: '/',
+  },
+
+  {
+    text: 'Profile',
+    img: 'marketplace',
+    path: '/profile',
+  },
+
+  {
+    text: 'Statistics',
+    img: 'saved',
+    path: '/statistics',
+  },
+
+  {
+    text: 'About Us',
+    img: 'ad',
+    path: '/about',
+  },
+];
