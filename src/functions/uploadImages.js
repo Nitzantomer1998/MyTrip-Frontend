d8c406cf0cb8d973f15e421e98036876a517ad2a
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-05-16 08:47:31 +0300

    Update uploadImages.js

--- src/functions/uploadImages.js
+++ src/functions/uploadImages.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
 export const uploadImages = async (formData, path, token) => {
   try {
@@ -8,7 +8,7 @@ export const uploadImages = async (formData, path, token) => {
       {
         headers: {
           Authorization: `Bearer ${token}`,
-          "content-type": "multipart/form-data",
+          'content-type': 'multipart/form-data',
         },
       }
     );
