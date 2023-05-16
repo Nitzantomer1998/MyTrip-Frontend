27e670fa8e9fb607774e542aaaaa6d9c00dee962
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-05-16 09:55:27 +0300

    Delete src directory

--- src/pages/profile/Liked.js
+++
@@ -1,7 +0,0 @@
-export default function Liked({ getAllPosts }) {
-  return (
-    <div>
-      <h1>liked</h1>
-    </div>
-  );
-}
--- src/svg/logo.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import logoPath from '../images/MyTrip_logo.png';
-
-function Logo() {
-  return <img src={logoPath} alt='MyTrip Logo' width='50' height='45' />;
-}
-
-export default Logo;
