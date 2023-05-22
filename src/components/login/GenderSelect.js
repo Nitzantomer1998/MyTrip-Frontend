72ec21b3caa513e14e3e13dd4c3f25a4c8e939d8
Nitzan <nitzantomer1998@gmail.com>
2023-05-22 10:01:05 +0300

    Updating gender selection

--- src/components/login/GenderSelect.js
+++ src/components/login/GenderSelect.js
@@ -1,58 +1,58 @@
-import { useMediaQuery } from "react-responsive";
+import { useMediaQuery } from 'react-responsive';
 
 export default function GenderSelect({ handleRegisterChange, genderError }) {
   const view1 = useMediaQuery({
-    query: "(min-width: 539px)",
+    query: '(min-width: 539px)',
   });
   const view2 = useMediaQuery({
-    query: "(min-width: 850px)",
+    query: '(min-width: 850px)',
   });
   const view3 = useMediaQuery({
-    query: "(min-width: 1170px)",
+    query: '(min-width: 1170px)',
   });
   return (
     <div
-      className="reg_grid"
-      style={{ marginBottom: `${genderError && !view3 ? "70px" : "0"}` }}
+      className='reg_grid'
+      style={{ marginBottom: `${genderError && !view3 ? '70px' : '0'}` }}
     >
-      <label htmlFor="male">
+      <label htmlFor='male'>
         Male
         <input
-          type="radio"
-          name="gender"
-          id="male"
-          value="male"
+          type='radio'
+          name='gender'
+          id='male'
+          value='male'
           onChange={handleRegisterChange}
         />
       </label>
-      <label htmlFor="female">
+      <label htmlFor='female'>
         Female
         <input
-          type="radio"
-          name="gender"
-          id="female"
-          value="female"
+          type='radio'
+          name='gender'
+          id='female'
+          value='female'
           onChange={handleRegisterChange}
         />
       </label>
-      <label htmlFor="custom">
-        Custom
+      <label htmlFor='non-binary'>
+        Non-Binary
         <input
-          type="radio"
-          name="gender"
-          id="custom"
-          value="custom"
+          type='radio'
+          name='gender'
+          id='non-binary'
+          value='non-binary'
           onChange={handleRegisterChange}
         />
       </label>
       {genderError && (
         <div
           className={
-            !view3 ? "input_error" : "input_error input_error_select_large"
+            !view3 ? 'input_error' : 'input_error input_error_select_large'
           }
         >
           <div
-            className={!view3 ? "error_arrow_bottom" : "error_arrow_left"}
+            className={!view3 ? 'error_arrow_bottom' : 'error_arrow_left'}
           ></div>
           {genderError}
         </div>
