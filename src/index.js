a1e5528fe347b876fb1ca1bbf6ce08621bd9383c
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-06-12 19:53:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import './styles/icons/icons.css';
 import App from './App';
@@ -8,13 +8,13 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './reducers';
+
 const store = createStore(rootReducer, composeWithDevTools());
 
-ReactDOM.render(
+createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </Provider>,
-  document.getElementById('root')
+  </Provider>
 );
